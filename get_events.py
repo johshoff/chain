cf6f6d67298e2d218ef14672200c884dcfdc55fd
Johannes Hoff <johshoff@gmail.com>
2014-05-10 20:01:42 -0700

    First pass on getting commit messages in

--- get_events.py
+++ get_events.py
@@ -9,12 +9,14 @@ import git_events
 import hg_events
 
 out_filename = 'sources.json'
+out_desc_filename = 'descriptions.json'
 
 if len(sys.argv) < 3:
 	print 'Usage: get_events.py <user> <repository_roots> [<additional repository roots> ...]'
 	print
 	print 'Repository roots will be scanned for hg and git repositories'
 	print 'Events will be written to %s' % out_filename
+	print 'Event descriptions will be written to %s' % out_desc_filename
 	print
 	print 'Only look at commits from <user>. This is just a substring match of the commit'
 	print "author's name or email address. Note that in git this is case sensitive."
@@ -37,11 +39,15 @@ for directory in search_dirs:
 				git_repos.append(root)
 
 event_dict = defaultdict(int)
+event_desc = defaultdict(list)
 
 def add_events(events):
 	global event_dict
 	for event in events:
 		event_dict[str(event['timestamp'])] += 1
+
+		if 'isodate' in event:
+			event_desc[event['isodate']].append(event)
 
 def repos_and_getters():
 	for repo in git_repos:
@@ -58,4 +64,5 @@ for repo, get_events in repos_and_getters():
 print 'Writing %d events to %s' % (len(event_dict), out_filename)
 
 json.dump(event_dict, open(out_filename, 'w'))
+json.dump(event_desc, open(out_desc_filename, 'w'))
 
--- hg_events.py
+++ hg_events.py
@@ -8,7 +8,7 @@ import os
 
 def get_events(cwd, user):
 	cwd = os.path.expanduser(cwd)
-	args = ['hg', 'log', '--user', user, '--template', '{date}\n']
+	args = ['hg', 'log', '--user', user, '--template', '{date|hgdate}\t{date|shortdate}\t{desc}\n']
 
 	stdout, stderr = Popen(args, cwd=cwd, stdout=PIPE).communicate()
 
@@ -16,7 +16,13 @@ def get_events(cwd, user):
 		return []
 
 	def fixup_line(line):
-		return {'timestamp':int(line.split('.')[0])}
+		items = line.split('\t')
+		return {
+				'timestamp': int(items[0].split(' ')[0]),
+				'isodate'  : items[1],
+				'desc'     : items[2],
+				'repo'     : os.path.split(cwd)[-1],
+			}
 
 	return [fixup_line(line) for line in stdout.strip().split('\n')]
 
