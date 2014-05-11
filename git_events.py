7146825a33ef090dc0ad0dd8207c10650f41f0d1
Johannes Hoff <johshoff@gmail.com>
2014-05-11 15:41:53 -0700

    Add commit messages for git repos as well

--- git_events.py
+++ git_events.py
@@ -8,7 +8,7 @@ import os
 
 def get_events(cwd, user):
 	cwd = os.path.expanduser(cwd)
-	args = ['git', 'log', '--author='+user, '--pretty=format:%at']
+	args = ['git', 'log', '--author='+user, '--pretty=format:%at\t%ai\t%s']
 
 	stdout, stderr = Popen(args, cwd=cwd, stdout=PIPE).communicate()
 
@@ -16,7 +16,13 @@ def get_events(cwd, user):
 		return []
 
 	def fixup_line(line):
-		return {'timestamp':int(line)}
+		items = line.split('\t')
+		return {
+				'timestamp': int(items[0]),
+				'isodate'  : items[1].split(' ')[0],
+				'desc'     : items[2],
+				'repo'     : os.path.split(cwd)[-1],
+			}
 
 	return [fixup_line(line) for line in stdout.strip().split('\n')]
 
